7c20630e5fc14e83dc8713407282087aede95656
Tomas Dirvanauskas <tomas@unity3d.com>
2023-02-13 14:15:48 +0200

    finalize

--- PluginResUpgrader/Assets/Editor/AndroidResUpgrader.cs
+++ PluginResUpgrader/Assets/Editor/AndroidResUpgrader.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Linq;
 using UnityEditor;
 using UnityEditor.Android;
-using UnityEditor.Callbacks;
+using System.Text;
 
 public class AndroidResUpgraderPostprocessor : IPostGenerateGradleAndroidProject
 {
@@ -24,13 +24,26 @@ public class AndroidResUpgraderPostprocessor : IPostGenerateGradleAndroidProject
 
         var result = EditorUtility.DisplayDialog($"Upgrade {AndroidResPath} folder ? ",
             $@"Starting Unity 2021.3 {AndroidResPath} folder can no longer be used for copying res files to gradle project, this has to be done either via android plugins or manually.
-Proceed with upgrade?",
+Proceed with upgrade? 
+('{AndroidResPath}' will be moved into '{AndroidResLegacyPath}')",
             "Yes",
             "No and don't ask again in this Editor session");
 
         if (!result)
         {
             SessionState.SetBool(AskAboutUpgradeResFolders, false);
+            return;
+        }
+
+        if (Directory.Exists(AndroidResLegacyPath))
+        {
+            EditorUtility.DisplayDialog(
+                "Upgrade failed",
+                @$"Cannot upgrade since '{AndroidResLegacyPath}' already exists, delete it.
+or manually merge '{AndroidResPath}' into '{AndroidResLegacyPath}' 
+and delete '{AndroidResPath}'.
+Restart Editor afterwards.",
+                "Ok");
             return;
         }
 
@@ -49,12 +62,19 @@ Proceed with upgrade?",
         if (!Directory.Exists(AndroidResLegacyPath))
             return;
 
+        var destination = Path.Combine(path, "src/main/res").Replace("\\", "/");
+        var log = new StringBuilder();
+        log.AppendLine("Legacy Android res files copying");
+        log.AppendLine($"Copying '{AndroidResLegacyPath}' -> '{destination}':");
         RecursiveCopy(new DirectoryInfo(AndroidResLegacyPath),
-            new DirectoryInfo(Path.Combine(path, "src/main/res").Replace("\\", "/")),
-            new[] {".meta"});
+            new DirectoryInfo(destination),
+            new[] {".meta"},
+            log);
+
+        Log(log.ToString());
     }
 
-    private static void RecursiveCopy(DirectoryInfo source, DirectoryInfo target, string[] ignoredExtensions)
+    private static void RecursiveCopy(DirectoryInfo source, DirectoryInfo target, string[] ignoredExtensions, StringBuilder log)
     {
         if (Directory.Exists(target.FullName) == false)
             Directory.CreateDirectory(target.FullName);
@@ -64,14 +84,14 @@ Proceed with upgrade?",
             if (ignoredExtensions.Contains(fi.Extension))
                 continue;
             var destination = Path.Combine(target.ToString(), fi.Name);
-            Log($"Copying {fi.FullName} -> {destination}");
+            log.AppendLine($" {fi.FullName} -> {destination}");
             fi.CopyTo(destination, true);
         }
 
         foreach (DirectoryInfo diSourceSubDir in source.GetDirectories())
         {
             DirectoryInfo nextTargetSubDir = target.CreateSubdirectory(diSourceSubDir.Name);
-            RecursiveCopy(diSourceSubDir, nextTargetSubDir, ignoredExtensions);
+            RecursiveCopy(diSourceSubDir, nextTargetSubDir, ignoredExtensions, log);
         }
     }
 }
