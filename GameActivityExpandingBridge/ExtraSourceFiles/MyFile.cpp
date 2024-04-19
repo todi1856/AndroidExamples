8caff7c36b91ee4cc3090fb78734eeef5da1a84e
Tomas Dirvanauskas <tomas@unity3d.com>
2024-04-19 14:55:14 +0300

    bump

--- GameActivityExpandingBridge/ExtraSourceFiles/MyFile.cpp
+++ GameActivityExpandingBridge/ExtraSourceFiles/MyFile.cpp
@@ -1,17 +1,17 @@
 #include "UGAApplication.h"
 #include "game-activity/native_app_glue/android_native_app_glue.h"
 
-void InitializeUserCode(Unity::UnityApplication* application)
+void UnityGameActivityPluginLoad(Unity::UnityApplication& application)
 {
-    application->GetEvents().Register<Unity::UnityEventProcessInput>([](const Unity::UnityEvent& e)
+    application.GetEvents().Register<Unity::UnityEventProcessInput>([](const Unity::UnityEventProcessInput& e)
     {
-        auto inputBuffer = static_cast<const Unity::UnityEventProcessInput&>(e).GetInputBuffer();
+        auto inputBuffer = e.GetInputBuffer();
 
         if (inputBuffer->motionEventsCount != 0) {
             for (uint64_t i = 0; i < inputBuffer->motionEventsCount; ++i) {
                 GameActivityMotionEvent* motionEvent = &inputBuffer->motionEvents[i];
                 if (motionEvent->action == AKEY_EVENT_ACTION_DOWN)
-                    e.GetApplication()->SendMessage("SendMessageReceiver", "SendMessageFromCpp", "HelloFromBridge");
+                    e.GetApplication().SendMessage("SendMessageReceiver", "SendMessageFromCpp", "HelloFromBridge");
             }
         }
     });
