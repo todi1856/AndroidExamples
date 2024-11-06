64602ebf9de7905322b6f7e5cef363ec594b996f
Rubix <tomdir@gmail.com>
2024-11-06 22:07:05 +0200

    cleanup

--- GalleryBrowser/Assets/Editor/AndroidGradleProjectModifier.cs
+++ GalleryBrowser/Assets/Editor/AndroidGradleProjectModifier.cs
@@ -1,5 +1,3 @@
-// Remove this, once Unity reexposes Configuration Manager
-#define DISABLE_PROJECT_FILES_MODIFIER
 #if UNITY_ANDROID
 using System.Collections.Generic;
 using System.IO;
@@ -88,30 +86,4 @@ class MyCustomBuildProcessor : IPostGenerateGradleAndroidProject
         PatchSettingsGradle(settingsGradle);
     }
 }
-#else
-public class AndroidGradleProjectModifier : AndroidProjectFilesModifier
-{
-    public override void OnModifyAndroidProjectFiles(AndroidProjectFiles projectFiles)
-    {
-        // Remove entries to launcher
-        projectFiles.LauncherBuildGradle.SetRaw(string.Empty);
-        foreach (var include in projectFiles.GradleSettings.IncludeList.GetElements())
-        {
-            if (include.GetRaw().Contains("launcher"))
-            {
-                include.Remove();
-                break;
-            }
-        }
-
-        // Remove launchable activities in unityLibrary
-        var manifest = projectFiles.UnityLibraryManifest.Manifest;
-        foreach (var item in manifest.GetActivitiesWithLauncherIntent())
-        {
-            foreach (var intent in item.IntentFilterList)
-                intent.Remove();
-        }
-    }
-}
-#endif
 #endif
